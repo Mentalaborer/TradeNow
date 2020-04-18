52c351b1856e971c74a873524505ca40e9a122fb
ShaziDoc <shonte.berkeley@gmail.com>
2020-04-18 21:13:17 +0000

    refactor arima

--- stock_forecast.R
+++ stock_forecast.R
@@ -13,15 +13,19 @@ library(tseries)
 library(quantmod)
 library(PerformanceAnalytics)
 
+# global options:
+lag.max = 10 # days to look back
+h = 5 # days to predict ahead (if change, then need to change dates) 
+historical_prices = 30 # x number of days of historical data to plot
 
 
 # load symbols
 
 # symbols is from the fetch_data script but should source directly, OR
-
+data_env <- new.env()
 
 # call a specific symbol directly
-focal_stock <- getSymbols('ABT', src = "yahoo", from = "2020-01-01",
+focal_stock <- getSymbols('FB', src = "yahoo", from = "2020-01-01",
            auto.assign = F, return.class = "xts", env = data_env)
 
 
@@ -31,10 +35,6 @@ colnames(focal_stock) <- paste("focal",
 
 # selct adjusted column only
 focal_stock_adjusted <- focal_stock$focal_Adjusted
-
-head(focal_stock_adjusted)
-plot(focal_stock_adjusted)
-
 
 plot(focal_stock_adjusted, col="darkred", main="focal_stock Price Series") 
 
@@ -50,24 +50,51 @@ summary(Stationarity_Diff)
 D.focal_stock_adjusted= focal_stock_adjusted-lag(focal_stock_adjusted)
 plot(D.focal_stock_adjusted, col="red4", main = "focal_stock_adjusted On First Differences")
 
+ggAcf(D.focal_stock_adjusted, lag.max = lag.max) + theme_bw()
 
-ggAcf(D.focal_stock_adjusted, lag.max = 10) + theme_bw()
+# Plot Autocorrelation
 
-#Auto ARIMA
+# How is the price today related to the past? 
+# Are the correlations large and positive for several lags? When decay?
+acf(focal_stock_adjusted, lag.max = lag.max, plot = T)
+
+## AUTOREGRESSIVE MODEL using AUTO ARIMA
+# Most widely used time series model
+# It’s like simple linear regression, each observation is regressed on the previous observation.
 
 # a function in R to do auto.arima, and it just goes through 
 # and test all these combinations and automatically 
 # selects a good choice for your p, d and q, your three parameters 
 # for your autoregressive component, your integrated component, and your moving average component.
 
-h = 5 # number of periods to look ahead for forecast (ex. next X days of stoc)
-fitted_arima = auto.arima(focal_stock_adjusted)
-arima_forecast =forecast(fitted_arima, h)
-arima_sweep = sw_sweep(arima_forecast)
-dates <- c(index(focal_stock_adjusted), index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 1, index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 2, index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 3, index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 4, index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 5)
-arima_sweep  = add_column(arima_sweep, dates)
-# Plotting only the passed 50 days of prices. 
-arima_sweep_display<- arima_sweep[(dim(arima_sweep)[1]-50):dim(arima_sweep)[1], ]
+fitted_arima <- auto.arima(focal_stock_adjusted)
+arima_forecast <- forecast(fitted_arima, h)
+arima_sweep <- sw_sweep(arima_forecast)
+
+# residual analysis:  How well does model fit the data?
+ts.plot(focal_stock_adjusted)
+ar_focal_fitted <- focal_stock_adjusted - residuals(arima_forecast)
+points(ar_focal_fitted, type = "l", col = "red", lty = 2)
+
+# prep for visualization of predictions
+
+# Note = h must match the dates here #
+dates <- c(index(focal_stock_adjusted), 
+           index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 1, 
+           index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 2, 
+           index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 3, 
+           index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 4, 
+           index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 5)
+        #  index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 6,
+        #  index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 7,
+         # index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 8,
+        #  index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 9,
+         # index(focal_stock_adjusted)[length(focal_stock_adjusted)] + 10)
+
+arima_sweep <- add_column(arima_sweep, dates)
+
+# Plotting historical prices (actual) and predictions
+arima_sweep_display<- arima_sweep[(dim(arima_sweep)[1]-historical_prices):dim(arima_sweep)[1], ]
 # Visualizing the forecast
 arima_sweep_display %>%
   ggplot(aes(x = dates, y = value, color = key)) +
@@ -78,12 +105,37 @@ arima_sweep_display %>%
               fill = "#596DD5", color = NA, size = 0, alpha = 0.8) +
   ## Actual & Forecast
   geom_line(size = 1) + 
-  #geom_point(size = 2) +
   ## Aesthetics
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  scale_x_date(date_breaks = "5 days", date_labels = "%b %d") +
-  labs(title = "focal_stock_adjusted 5 Day Ahead ARIMA Price Forecast", x = "", y = "")
+  scale_x_date(date_breaks = "2 days", date_labels = "%b %d") +
+  labs(title = "focal_stock_adjusted 5 Days Ahead ARIMA Price Forecast", x = "", y = "")
+
+# print predictions only
+arima_sweep_display %>% 
+  select(key, value, lo.95, hi.95, dates) %>%
+  filter(key == 'forecast')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
